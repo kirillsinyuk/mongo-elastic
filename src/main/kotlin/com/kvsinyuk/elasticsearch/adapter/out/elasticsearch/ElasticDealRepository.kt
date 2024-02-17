659029c3dfe0951f502549984cde577de7934870
Kirill Sinyuk <kirill.s.works@gmail.com>
2024-02-17 12:59:21 +0300

    #17 add elasticsearch search handle

---
+++ src/main/kotlin/com/kvsinyuk/elasticsearch/adapter/out/elasticsearch/ElasticDealRepository.kt
@@ -0,0 +1,14 @@
+package com.kvsinyuk.elasticsearch.adapter.out.elasticsearch
+
+import com.kvsinyuk.elasticsearch.domain.Deal
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.elasticsearch.annotations.Query
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
+
+interface ElasticDealRepository : ElasticsearchRepository<Deal, String> {
+
+    @Query(query = "{ \"multi_match\" : {\"query\" : \"?0\" }}")
+    //@Query(query = "\"query\": {\"bool\": {\"must\": [{ \"match\": {\"userId\": ?0}}]}}")
+    fun searchAll(search: String, page: Pageable): Page<Deal>
+}
---
+++ src/main/kotlin/com/kvsinyuk/elasticsearch/config/ElasticsearchConfiguration.kt
@@ -0,0 +1,10 @@
+package com.kvsinyuk.elasticsearch.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = ["com.kvsinyuk.elasticsearch.adapter.out.elasticsearch"])
+@ComponentScan(basePackages = [ "com.kvsinyuk.elasticsearch" ])
+class ElasticsearchConfiguration
