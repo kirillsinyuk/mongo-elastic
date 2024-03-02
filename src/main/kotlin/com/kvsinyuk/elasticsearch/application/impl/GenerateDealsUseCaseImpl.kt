8492b8b40ecdb4df3c4f4ec192913337d8fcd655
Kirill Sinyuk <kirill.s.works@gmail.com>
2024-03-02 13:03:36 +0300

    #24 refactoring

--- src/main/kotlin/com/kvsinyuk/elasticsearch/application/impl/GenerateDealsUseCaseImpl.kt
+++ src/main/kotlin/com/kvsinyuk/elasticsearch/application/impl/GenerateDealsUseCaseImpl.kt
@@ -3,8 +3,7 @@ package com.kvsinyuk.elasticsearch.application.impl
 import com.kvsinyuk.elasticsearch.adapter.out.mongo.MongoDealRepository
 import com.kvsinyuk.elasticsearch.application.port.GenerateDealsUseCase
 import com.kvsinyuk.elasticsearch.utils.generateDeal
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
@@ -16,12 +15,14 @@ class GenerateDealsUseCaseImpl(
     @Value("\${generator.batch.count}")
     private var batchCount: Int = 20
 
-    override fun generateDeals(dealAmount: Int) {
-        for (i in 0 until dealAmount step batchCount) {
-            CoroutineScope(Dispatchers.Default).launch {
-                (0 until batchCount)
-                    .map { generateDeal() }
-                    .let { dealRepository.saveAll(it) }
+    override suspend fun generateDeals(dealAmount: Int) {
+        coroutineScope {
+            for (i in 0 until dealAmount step batchCount) {
+                launch {
+                    (0 until batchCount)
+                        .map { generateDeal() }
+                        .let { dealRepository.saveAll(it) }
+                }
             }
         }
     }
